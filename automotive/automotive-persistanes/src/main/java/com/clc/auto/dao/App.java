45686dcf4de21a64718fe545579f6baf915cc557
AdminSpace <30335737+AdminSpace@users.noreply.github.com>
2019-03-13 14:52:24 +0530

    Add files via upload

---
+++ automotive/automotive-persistanes/src/main/java/com/clc/auto/dao/App.java
@@ -0,0 +1,13 @@
+package com.clc.auto.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ automotive/automotive-services/src/main/java/com/clc/auto/service/App.java
@@ -0,0 +1,13 @@
+package com.clc.auto.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
